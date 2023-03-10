6d1956d131fdbf6e7d6c2e72e9dbb567f4b0f6e2
Dragos Popescu <dragos.andreip@yahoo.com>
2023-03-10 15:35:22 +0100

    Added test atlas

--- mlw/third/microui/microui.frag.glsl
+++ mlw/third/microui/microui.frag.glsl
@@ -10,4 +10,6 @@ void main() {
     //FragmentColor.rgb = texture(atlas, TexCoords).a * VertexColor.rgb;
     //FragmentColor.a = texture(atlas, TexCoords).a;
     FragmentColor = texture(atlas, TexCoords) * VertexColor;
+    //FragmentColor = texture(atlas, vec2(0.0938, 0.6250));
+    //FragmentColor = vec4(texture(atlas, vec2(0.02422, 1.0 - 0.6016)).aaa, 1);
 }
